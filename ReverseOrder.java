a49e82f0dc16fa004ea5524928630c24f1f2bcff
Surya K M <115336797+SuryaKM23@users.noreply.github.com>
2023-06-08 09:29:50 +0530

    Add files via upload

---
+++ ReverseOrder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+  public class ReverseOrder {
+  public static void main(String[] args) {
+     TreeSet<String> t_set = new TreeSet<String>();
+          t_set.add("Red");
+          t_set.add("Green");
+          t_set.add("Black");
+          t_set.add("Pink");
+          t_set.add("orange");
+   System.out.println("Original tree set:" + t_set);  
+   System.out.println("Elements in Reverse Order:");
+   Iterator its= t_set.;
+     while (its.hasNext()) {
+        System.out.println(its.next());
+     }
+     Iterator it = t_set.descendingIterator();
+     System.out.println("Elements in Reverse Order:");
+     while (it.hasNext()) {
+        System.out.println(it.next());
+     }
+  }
+}
